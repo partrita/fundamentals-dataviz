387018e284dffe2ef39a00a750775250d98f255a
fkt <deepthought@postech.ac.kr>
2026-02-22 19:50:53 +0900

    feat: Adopt Noto Sans KR font for Korean book chapters by adding font files and updating configurations.

--- mybook/_common.R
+++ mybook/_common.R
@@ -10,10 +10,17 @@ knitr::opts_chunk$set(
   fig.align = 'center',
   fig.width = 6,
   fig.asp = 0.618,  # 1 / phi
-  fig.show = "hold"
+  fig.show = "hold",
+  dev = "ragg_png"
 )
 
 options(dplyr.print_min = 6, dplyr.print_max = 6)
 
 library(dviz.supp)
+
+# Define font families to use Noto Sans KR
+dviz_font_family <- "Noto Sans KR"
+dviz_font_family_bold <- "Noto Sans KR"
+dviz_font_family_condensed <- "Noto Sans KR"
+dviz_font_family_bold_condensed <- "Noto Sans KR"
 
